e3f24f46bb8ac8f6b077b22f1d05993b0d8a67f3
Johnson Domcasse <j.domacasse@student.fontys.nl>
2024-05-15 18:52:45 +0200

    sensor data being sent to node red

--- ESP sending/ESP sending.ino
+++ ESP sending/ESP sending.ino
@@ -1,14 +1,74 @@
-#define RXp2 16
-#define TXp2 17
+#include "DHTesp.h"
+#include "WiFi.h"
+#include "PubSubClient.h"
+#include <ArduinoJson.h>
+
+#define MOISTURE_SENSOR_PIN 32
+#define MOISTURE_SENSOR_MAX 4095 //dry
+#define MOISTURE_SENSOR_MIN 2375 //fully submerged
+#define INTERVAL 1000
+
+const char* ssid = "MQTT Test";
+const char* password = "tomato45";
+const char* mqtt_server = "test.mosquitto.org";
+const int mqtt_port = 1883;
+const char* mqtt_topic = "sensor_data";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+int sensor_value = 0;
+unsigned long previousMillis = 0;
+
+/// @brief Function that sets up the wifi connection.
+void setup_wifi() {
+  delay(10);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+/// @brief Function to check if the ESP is reconnecting (debugging purposes)
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect("ESP32Client")) {
+    } else {
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(9600);
-  Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
-  
+  Serial2.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
 }
 
 void loop() {
-  Serial.println("hello boss");
-  Serial2.println("ON");
-  delay(1000);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= INTERVAL){
+    previousMillis = currentMillis;
+
+    int sensor_reading = analogRead(MOISTURE_SENSOR_PIN);
+    sensor_value = map(sensor_reading, MOISTURE_SENSOR_MAX, MOISTURE_SENSOR_MIN, 0, 100); //mapped from 0 to 100% based on sensor max values.
+
+    StaticJsonDocument<200> doc;
+    doc["moisture"] = sensor_value;
+    String jsonStr;
+    serializeJson(doc, jsonStr);
+
+    client.publish(mqtt_topic, jsonStr.c_str());
+
+    Serial2.println("ON");
+    
+    //debug
+    //Serial.println("hello boss");
+  }
 }
--- moist sens/Moisture sensor/src/main.cpp
+++ moist sens/Moisture sensor/src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 
 #define MOISTURE_SENSOR_PIN A0
-#define MOISTURE_SENSOR_MAX 750
+#define MOISTURE_SENSOR_MAX 4095
+#define MOISTURE_SENSOR_MIN 2485
 
 /*
   80 ~ 100; fully submerged in liquid
@@ -14,7 +15,7 @@ void setup() {
 int sensor_value = 0;
 void loop() {
   int sensor_reading = analogRead(MOISTURE_SENSOR_PIN);
-  sensor_value = map(sensor_reading, 0, MOISTURE_SENSOR_MAX, 0, 100); //mapped from 0 to 100% based on sensor max values.
+  sensor_value = map(sensor_reading, MOISTURE_SENSOR_MAX, MOISTURE_SENSOR_MIN, 0, 100); //mapped from 0 to 100% based on sensor max values.
   Serial.print("moisture sensor value: ");
   Serial.print(sensor_value);
   Serial.println("%");
