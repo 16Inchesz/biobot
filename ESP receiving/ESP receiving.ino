96492582bb9fcdaa0fd756eb054f6a3915185a28
Johnson Domcasse <j.domacasse@student.fontys.nl>
2024-05-16 14:34:22 +0200

    serial ESP works

--- ESP receiving/ESP receiving.ino
+++ ESP receiving/ESP receiving.ino
@@ -1,25 +1,16 @@
 #include "WiFi.h"
 #include "PubSubClient.h"
-#include <ArduinoJson.h>
-
-#define MOISTURE_SENSOR_PIN 32
-#define MOISTURE_SENSOR_MAX 4095 //dry
-#define MOISTURE_SENSOR_MIN 2375 //fully submerged
-#define INTERVAL 1000
 
 const char* ssid = "MQTT Test";
 const char* password = "tomato45";
 const char* mqtt_server = "broker.hivemq.com";
 const int mqtt_port = 1883;
-const char* mqtt_topic = "sensor_data";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-int sensor_value = 0;
-unsigned long previousMillis = 0;
+String previousMessage;
 
-/// @brief Function that sets up the wifi connection.
 void setup_wifi() {
   delay(10);
   Serial.println();
@@ -35,12 +26,40 @@ void setup_wifi() {
   Serial.println("WiFi connected");
 }
 
-/// @brief Function to check if the ESP is reconnecting (debugging purposes)
+void callback(String topic, byte* message, unsigned int length){
+  Serial.print("message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i=0; i<length-1; i++){
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  if(topic == "soil_type"){
+    if (messageTemp != previousMessage){
+      Serial.print("message received: ");
+      if (messageTemp == "Dirt"){
+        Serial2.println("ON");
+      } else if (messageTemp == "Sand") {
+        Serial2.println("ON");
+      } else {
+        Serial.println("Nothing to be done");
+      }
+    }
+  }
+  previousMessage = messageTemp;
+  Serial.println();
+}
+
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("ESP32Client")) {
       Serial.println("connected");
+      client.subscribe("soil_type");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -52,8 +71,10 @@ void reconnect() {
 
 void setup() {
   Serial.begin(9600);
+  Serial2.begin(9600);
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
 void loop() {
@@ -62,25 +83,4 @@ void loop() {
   }
 
   client.loop();
-
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= INTERVAL){
-    previousMillis = currentMillis;
-
-    int sensor_reading = analogRead(MOISTURE_SENSOR_PIN);
-    sensor_value = map(sensor_reading, MOISTURE_SENSOR_MAX, MOISTURE_SENSOR_MIN, 0, 100); //mapped from 0 to 100% based on sensor max values.
-
-    StaticJsonDocument<200> doc;
-    doc["moisture"] = sensor_value;
-    String jsonStr;
-    serializeJson(doc, jsonStr);
-
-    client.publish(mqtt_topic, jsonStr.c_str());
-    Serial.println(sensor_value);
-
-    Serial2.println("ON");
-    
-    //debug
-    //Serial.println("hello boss");
-  }
 }
